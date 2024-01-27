be380264425589e753e72aee4eba5ceeecace29d
ksaito <kitsit.39@gmail.com>
2024-01-27 11:18:58 +0900

    タイヤの影響を考慮する

--- gear.rb
+++ gear.rb
@@ -1,12 +1,19 @@
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog       = cog
+    @rim       = rim
+    @tire      = tire
   end
 
   def ratio
     chainring / cog.to_f
+  end
+
+  def gear_inches
+    # タイヤはリムの回りを囲むので、直径を計算するためには2倍する
+    ratio * (rim + (tire * 2))
   end
 end
